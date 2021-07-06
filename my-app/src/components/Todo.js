b324d495867403c07c90eb3c57c7b596866cd5e6
ksdevinney <katie@devinney.org>
2021-07-06 16:15:24 -0500

    open and close modals

--- my-app/src/components/Todo.js
+++ my-app/src/components/Todo.js
@@ -9,14 +9,18 @@ function Todo(props) {
         setShowModal(true);
     }
 
+    function closeModalHandler() {
+        setShowModal(false);
+    }
+
     return(
         <div className="card">
         <h2>{props.text}</h2>
         <div className="actions">
           <button className="btn" onClick={deleteHandler} >Delete</button>
         </div>
-        { showModal && <Modal /> }
-        { showModal && <Backdrop /> }
+        { showModal && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} /> }
+        { showModal && <Backdrop onClick={closeModalHandler} /> }
       </div>
     )
 }
